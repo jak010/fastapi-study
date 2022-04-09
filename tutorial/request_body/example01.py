e838b789cf1cb7eefefa81f71a966e783f2ec099
jak010 <bluetoon@naver.com>
2022-04-09 15:35:29 +0900

    note: request_body append

--- tutorial/request_body/example01.py
+++ tutorial/request_body/example01.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, Form
 
 from pydantic import BaseModel
 
+app = FastAPI()
+
+
 # 이 구조는 raw json을 통해 보낼 수 있음
 
 class Item(BaseModel):
@@ -12,9 +15,7 @@ class Item(BaseModel):
     tax: Optional[float] = None
 
 
-app = FastAPI()
-
-
 @app.post("/items")
 def create_item(item: Item):
+    print(item.name.capitalize())
     return item
